a63ae9ea7d11dd52415cfcefd05304c7905be588
Elyssana Oliveira <53406214+Elyssana@users.noreply.github.com>
2021-07-10 00:56:32 -0300

    Update fibonacci-rec.c

--- Lista 01/fibonacci-rec.c
+++ Lista 01/fibonacci-rec.c
@@ -1,45 +1,43 @@
 // 1) Faça um programa que exibe na tela os elementos de uma série de fibonacci de forma imperativa e recursiva, depois calcule a complexidade de cada algoritmo.
 
 #include <stdio.h>
-
+int cont = 0;
 int Fibonacci(int n)
 {
-
-    if(n <= 2)
+    if (n <= 2) 
     {
-        return 1;
+        return 1;//(1)
     }
-    else
-    {
+    else {
         return Fibonacci(n - 1) + Fibonacci(n - 2);
+        //printf("(%d)", cont++);
+        //return result;
     }
-        
-}
+}//(2^n)+1
 
-void loop( int n)
+void loop(int n)
 {
-    if(n == 0)
+    if (n == 0) //(1)
     {
         return;
     }
 
     else
     {
-        loop(n-1); 
-        
-        printf(" %d ", Fibonacci(n));
+        loop(n - 1); //(n)
+
+        printf(" %d ", Fibonacci(n)); //(2^n)
     }
+}//(n)*(2^n)
 
-}
-
-int main(){
+int main()
+{
 
     int n;
 
     printf("Digite um numero: ");
 
-    scanf("%d", &n); 
+    scanf("%d", &n);
 
-    loop(n); 
-    
-}
+    loop(n);
+}//O((2^n)*(n))
