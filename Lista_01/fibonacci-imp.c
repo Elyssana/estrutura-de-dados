ba75d2bdcd0937e8cca82ab91bbac53f4d63caa3
Elyssana Oliveira <53406214+Elyssana@users.noreply.github.com>
2021-07-18 20:31:12 -0300

    Create fibonacci-imp.c

---
+++ Lista_01/fibonacci-imp.c
@@ -0,0 +1,46 @@
+// 1) Faça um programa que exibe na tela os elementos de uma série de fibonacci de forma imperativa e recursiva, depois calcule a complexidade de cada algoritmo.
+#include <stdio.h>
+
+
+int Fibonacci(int n)
+{
+    if(n <= 2)
+    {
+        return 1;
+    }
+    else
+    {
+        int ult=1, penul=1, cont, atual=0;
+
+        for (cont = 2; cont < n; cont++){
+
+            atual = ult + penul;
+            penul = ult;
+            ult = atual;
+            // printf("\n atual  =%d\n", atual);
+            
+        }
+
+        return atual;
+    }
+        
+}
+
+
+int main(){
+
+    int n;
+
+    printf("Digite a quantidade de numeros da série: ");
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)//n
+    {
+        
+        printf("%d ", Fibonacci(i));
+
+        
+    }
+    
+}
