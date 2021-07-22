5ca809e73c77ec4e7f665b2a6c657d8a321bf697
Elyssana Oliveira <53406214+Elyssana@users.noreply.github.com>
2021-07-22 20:18:33 -0300

    Update questao1_b.c

--- Lista_02/questao1_b.c
+++ Lista_02/questao1_b.c
@@ -8,9 +8,8 @@ int criaPilha()
     return topo;
 }
 
-
 int empilha(int pilha[], int topo, int elem, int max)
-{    
+{
     if (topo == max)
     {
         printf("Pilha cheia!");
@@ -22,7 +21,6 @@ int empilha(int pilha[], int topo, int elem, int max)
         pilha[topo] = elem;
     }
     return topo;
-
 }
 
 int desempilha(int pilha[], int topo)
@@ -34,81 +32,69 @@ int desempilha(int pilha[], int topo)
     return topo;
 }
 
-
 void parenteses(int topo, int i, int pilha[], char str[])
 {
-    // printf("%s\n", str);
 
-    if(topo < -1)
+    if (topo < -1)
     {
         printf("1º excesso de ')' na posição: \t%d \n", i);
-       
-    }    
+    }
 
-    else if(topo > -1)
+    else if (topo > -1)
     {
-        if(topo > 0)
+        if (topo > 0)
         {
-            while(topo > 0)
+            while (topo > 0)
             {
                 desempilha(pilha, topo);
                 topo--;
-
             }
         }
         printf("1º excesso '(' na posição: \t%d \n", pilha[topo]);
     }
-    else if(topo == -1)
+    else if (topo == -1)
     {
         printf("parenteses balanceados!\n");
     }
 }
 
-
 void verifica(char str[], int pilha[], int max)
 {
     int i, topo = criaPilha();
 
-   
-
-    for(i = 0; i < max; i++)
+    for (i = 0; i < max; i++)
     {
-        if(str[i] == '(')
+        if (str[i] == '(')
         {
             topo = empilha(pilha, topo, i, max);
-
-            
         }
-        else if(str[i] == ')')
+        else if (str[i] == ')')
         {
             topo = desempilha(pilha, topo);
 
-            
-            if(topo < -1)
+            if (topo < -1)
             {
 
                 break;
-            }   
-
-                    
+            }
         }
     }
 
     parenteses(topo, i, pilha, str);
-} 
+}
 
+int main()
+{
 
-int main(){
+    while(1)
+    {
+        char str[100];
+        int pilha[100];
 
+        scanf("%s", str);
 
-    char str[100];
-    int pilha[100];
+        int tam = strlen(str);
 
-    scanf("%s", str);
-
-    int tam = strlen(str);
-
-
-    verifica(str, pilha, tam);
-
+        verifica(str, pilha, tam);
+    }
 }
